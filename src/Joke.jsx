e3c5074898f4c9d429f13812fc61f7e0c4982fd5
NHGamer <aniket94sharma56@gmail.com>
2026-03-19 02:46:10 +0530

    updated design

--- src/Joke.jsx
+++ src/Joke.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 const Joke = (props) => {
   return (
     <div>
+      <br />
+      <h2>Generate a Joke: </h2>
+      <br />
       <h3>{props.setup}</h3>
       <h3>{props.punchline}</h3>
+      <br />
     </div>
   )
 }
